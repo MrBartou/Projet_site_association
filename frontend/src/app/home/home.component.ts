d89665429c087ba11f1f32eabc0f23fdd8cb99fb
Anthony <anthony.denin@epitech.eu>
2021-05-09 18:09:38 +0200

    feat: polish

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  images = ["image1.png", "image2.png", "image3.png", "image4.png"].map((n) => `../../assets/home/${n}`);
 
   constructor() { }
 
