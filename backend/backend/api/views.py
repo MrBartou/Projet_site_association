3a4dbc7c17e909fa57e731c61fcf15974dcb799d
maxoft <leo.bazot@epitech.eu>
2021-06-04 15:40:27 +0200

    Added model list modifications for admins

--- backend/backend/api/views.py
+++ backend/backend/api/views.py
@@ -21,3 +21,41 @@ class SubsciptionViewSet(viewsets.ModelViewSet):
 class AdminsViewSet(viewsets.ModelViewSet):
     queryset = Admins.objects.all()
     serializer_class = AdminsSerializer
+
+@api_view(['POST'])
+def activity_list(request):
+    if request.method == 'POST':
+        serializer = EventsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def suppr_acti(request, title):
+    try:
+        even = Events.objects.get(title=title)
+    except Events.DoesNotExist:
+        return JsonResponse({'message': 'The Events does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'DELETE':
+        even.delete()
+        return JsonResponse({'message': 'Events was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['POST'])
+def user_list(request):
+    if request.method == 'POST':
+        serializer = SubsciptionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def partnership_list(request):
+    if request.method == 'POST':
+        serializer = PartnershipSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
