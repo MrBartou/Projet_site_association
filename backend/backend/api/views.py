4318b2932da225846b55b32392a2c4fab7e80958
maxoft <leo.bazot@epitech.eu>
2021-06-04 15:08:51 +0200

    Added Admin Viewset

--- backend/backend/api/views.py
+++ backend/backend/api/views.py
@@ -1,7 +1,10 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from .serializers import *
 from .models import *
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.http.response import JsonResponse
 
 class EventsViewSet(viewsets.ModelViewSet):
     queryset = Events.objects.all()
@@ -14,3 +17,7 @@ class PartnershipViewSet(viewsets.ModelViewSet):
 class SubsciptionViewSet(viewsets.ModelViewSet):
     queryset = Subsciption.objects.all()
     serializer_class = SubsciptionSerializer
+
+class AdminsViewSet(viewsets.ModelViewSet):
+    queryset = Admins.objects.all()
+    serializer_class = AdminsSerializer
