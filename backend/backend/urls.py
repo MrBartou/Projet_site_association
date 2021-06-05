00459ad5cede9601505711cd7fedf96db8437e9a
maxoft <leo.bazot@epitech.eu>
2021-06-05 21:53:53 +0200

    Added urls doc

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,3 +1,4 @@
+"""URL path and rules to access backend elements"""
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
@@ -5,16 +6,15 @@ from rest_framework import routers
 from backend.api import views
 
 router = routers.DefaultRouter()
-'''
-event -> road for list event
-partnership -> road for list partnership
-subscription -> road for list subscription
-admins -> road for list admins
-'''
+
 router.register(r'event', views.EventsViewSet)
+"""road to access event list"""
 router.register(r'partnership', views.PartnershipViewSet)
+"""road to access partnership list"""
 router.register(r'subscription', views.SubsciptionViewSet)
+"""road to access subscription list"""
 router.register(r'admins', views.AdminsViewSet)
+"""road to access admin's login and management page"""
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +25,4 @@ urlpatterns = [
     url(r'^partnerships/',views.partnership_list),
     url(r'events/(?P<title>[A-Z\-]+)$', views.suppr_acti),
 ]
+"""describe patterns to access diferents roads"""
